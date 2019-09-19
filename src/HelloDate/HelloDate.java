457c5b6cc9d2df23c523c3fb62b8c8ac6d1534a0
Miсhail h2kb Bereza <berezama@gmail.com>
2019-09-19 20:27:54 +0300

    added HelloDate and ShowProperties.

---
+++ src/HelloDate/HelloDate.java
@@ -0,0 +1,10 @@
+package HelloDate;
+
+import java.util.*;
+
+public class HelloDate {
+    public static void main(String[] args) {
+        System.out.println("Hello, today is: ");
+        System.out.println(new Date());
+    }
+}
