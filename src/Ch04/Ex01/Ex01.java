ec0342ebfe0e166a5663dfaf3fd4c63397395fc4
Miсhail h2kb Bereza <berezama@gmail.com>
2019-09-23 16:09:15 +0800

    Added chapter 04 exercise 01.

---
+++ src/Ch04/Ex01/Ex01.java
@@ -0,0 +1,9 @@
+package Ch04.Ex01;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 100; i++) {
+            System.out.println(i + " ");
+        }
+    }
+}
---
+++ src/Ch04/Ex01/readme.txt
@@ -0,0 +1,1 @@
+Write a program to print values from one to 100.
