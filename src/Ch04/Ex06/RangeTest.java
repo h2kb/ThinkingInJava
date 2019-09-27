88b4d8c8a2aeb6aa6432bf9e364b54cd5f01e909
Miсhail h2kb Bereza <berezama@gmail.com>
2019-09-27 08:40:15 +0800

    Added Chapter 4 Exercise 6.

---
+++ src/Ch04/Ex06/RangeTest.java
@@ -0,0 +1,17 @@
+package Ch04.Ex06;
+
+public class RangeTest {
+    public static boolean test(int testval, int begin, int end) {
+        if(testval >= begin && testval <= end) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(test(10,5,15));
+        System.out.println(test(5,10,15));
+        System.out.println(test(5,5,5));
+    }
+}
---
+++ src/Ch04/Ex06/readme.txt
@@ -0,0 +1,1 @@
+Modify the two test() methods in the previous two programs so they take two extra arguments, begin and end, and so testval is tested to see if it is within the range between (and including) begin and end.
